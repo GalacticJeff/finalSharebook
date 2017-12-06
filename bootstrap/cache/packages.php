fc66cae62ed93832a793efce59b85be6f4ca4d2b
jeff <simon.dev93@gmail.com>
2017-12-06 10:23:30 -0400

    actualizado

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'jeroennoten/laravel-adminlte' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'JeroenNoten\\LaravelAdminLte\\ServiceProvider',
+    ),
+  ),
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
+);
---
+++ storage/framework/views/fc2dff2d1a7b509c5e1d729bba7f11965ff73077.php
@@ -0,0 +1,10 @@
+<?php $__env->startSection('title', 'AdminLTE'); ?>
+
+<?php $__env->startSection('content_header'); ?>
+    
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+    
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('adminlte::page', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
